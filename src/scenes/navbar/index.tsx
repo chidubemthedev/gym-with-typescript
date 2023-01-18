a093294a23ee8937d0c1e4334cec22d41e56825c
Trillzdboy <chukwudubem7@gmail.com>
2023-01-18 10:13:26 +0100

    added enum typing for the selected pages

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,23 +1,63 @@
-import React from 'react'
-import {Bars3Icon, XMarkIcon} from  '@heroicons/react/24/solid'
-import Logo from '@/assets/Logo.png'
+import React from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "@/assets/Logo.png";
+import Link from "./Link";
+import { SelectedPage } from "@/shared/types";
 
-type Props = {}
+type Props = {
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-const Navbar = (props: Props) => {
-    const flexBetween = 'flex items-center justify-between'
+const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+  const flexBetween = "flex items-center justify-between";
 
   return (
     <nav>
-        <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
-            <div className={`${flexBetween} mx-auto w-5/6`}>
-                <div className={`${flexBetween} w-full gap-16`}>
-                    <img src={Logo} alt="Logo" />
-                </div>
-            </div>
-        </div>
-    </nav>
-  )
-}
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          {/* Left side */}
+          <div className={`${flexBetween} w-full gap-16`}>
+            <img src={Logo} alt="Logo" />
+          </div>
+          {/* Right side */}
+          <div className={`${flexBetween} w-full`}>
+            {/* Inner left */}
+            <div className={`${flexBetween} gap-8 text-sm`}>
+              <Link
+                page="Home"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
 
-export default Navbar
+              <Link
+                page="Benefits"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+
+              <Link
+                page="Our Classes"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+
+              <Link
+                page="Contact Us"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+            </div>
+            {/* Inner right */}
+            <div className={`${flexBetween} gap-8`}>
+              <p>Sign In</p>
+              <button>Become a Member</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
