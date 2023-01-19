64bbf362e9cb22da281843492911b8e136199e03
Trillzdboy <chukwudubem7@gmail.com>
2023-01-19 08:57:11 +0100

    fixed variation

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -19,55 +19,49 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
     <nav>
       <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
         <div className={`${flexBetween} mx-auto w-5/6`}>
-          {/* Left side */}
           <div className={`${flexBetween} w-full gap-16`}>
-            <img src={Logo} alt="Logo" />
+            {/* LEFT SIDE */}
+            <img alt="logo" src={Logo} />
+
+            {/* RIGHT SIDE */}
+            {isAboveMediumScreens ? (
+              <div className={`${flexBetween} w-full`}>
+                <div className={`${flexBetween} gap-8 text-sm`}>
+                  <Link
+                    page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Benefits"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Our Classes"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Contact Us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                </div>
+                <div className={`${flexBetween} gap-8`}>
+                  <p>Sign In</p>
+                  <button>Become a Member</button>
+                </div>
+              </div>
+            ) : (
+              <button
+                className="rounded-full bg-secondary-500 p-2"
+                onClick={() => setIsMenuToggled(!isMenuToggled)}
+              >
+                <Bars3Icon className="h-6 w-6 text-white" />
+              </button>
+            )}
           </div>
-          {/* Right side */}
-          {isAboveMediumScreens ? (
-            <div className={`${flexBetween} w-full`}>
-              {/* Inner left */}
-              <div className={`${flexBetween} gap-8 text-sm`}>
-                <Link
-                  page="Home"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-
-                <Link
-                  page="Benefits"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-
-                <Link
-                  page="Our Classes"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-
-                <Link
-                  page="Contact Us"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-              </div>
-              {/* Inner right */}
-              <div className={`${flexBetween} gap-8`}>
-                <p>Sign In</p>
-                <button>Become a Member</button>
-              </div>
-            </div>
-          ) : (
-            <button
-              className="rounded-full bg-secondary-500 p-2"
-              onClick={() => {
-                setIsMenuToggled(!isMenuToggled);
-              }}
-            >
-              <Bars3Icon className="h-6 w-6 text-white" />
-            </button>
-          )}
         </div>
       </div>
     </nav>
