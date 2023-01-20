ff456606915be1858d55751acacef175d523c4f3
Trillzdboy <chukwudubem7@gmail.com>
2023-01-20 10:36:12 +0100

    finished home component

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,14 +1,78 @@
-import { SelectedPage } from '@/shared/types'
-import React from 'react'
+import React from "react";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import { SelectedPage } from "@/shared/types";
+import ActionButton from "@/shared/ActionButton";
+import HomePageText from "@/assets/HomePageText.png";
+import HomePageGraphic from "@/assets/HomePageGraphic.png";
+import SponsorRedBull from "@/assets/SponsorRedBull.png";
+import SponsorFortune from "@/assets/SponsorFortune.png";
+import SponsorForbes from "@/assets/SponsorForbes.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
-    setSelectedPage: (value: SelectedPage) => void
-}
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-const Home = ({setSelectedPage}: Props) => {
+const Home = ({ setSelectedPage }: Props) => {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 600px)");
+
   return (
-    <div>Home</div>
-  )
-}
+    <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+      {/* Image and main header */}
+      <div className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6">
+        {/* Main header */}
+        <div className="z-10 mt-32 basis-3/5 ">
+          {/* Headings */}
+          <div className="md:-mt-20">
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext ">
+                <img src={HomePageText} alt="home-page-text" />
+              </div>
+            </div>
 
-export default Home
+            <p className="mt-8 text-sm">
+              Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
+              Studios to get the Body Shapes That you Dream of.. Get Your Dream
+              Body Now.
+            </p>
+          </div>
+
+          {/* Actions */}
+          <div className="mt-8 flex items-center gap-8">
+            <ActionButton setSelectedPage={setSelectedPage}>
+              {" "}
+              Join Now{" "}
+            </ActionButton>
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
+          </div>
+        </div>
+
+        {/* Image */}
+        <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-end">
+          <img src={HomePageGraphic} alt="home-page-graphic" />
+        </div>
+      </div>
+
+      {/* Sponsors */}
+      {isAboveMediumScreens && (
+        <div className="h-[150px] w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8">
+              <img src={SponsorRedBull} alt="red-bull-sponsor" />
+              <img src={SponsorForbes} alt="red-bull-forbes" />
+              <img src={SponsorFortune} alt="red-bull-fortune" />
+            </div>
+          </div>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default Home;
