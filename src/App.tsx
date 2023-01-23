8e7bcda247ced65a8f9d3dc35780837fb861ca9d
Trillzdboy <chukwudubem7@gmail.com>
2023-01-23 10:07:03 +0100

    added finished comtact section

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { SelectedPage } from "@/shared/types";
 import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
 import OurClasses from "@/scenes/ourClasses";
-import ContactUs from "@/scenes/ContactUs";
+import ContactUs from "@/scenes/contactUs";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
